f3b17a5faee75f6f48be45149cf782a0317b8b18
Gokmen Balkan <gokmenbalkan@gmail.com>
2015-08-30 16:04:04 +0300

    Created Calculator.java class to do mathematical operations Created Operator.java enumeration to hold values for valid operations Finished layout of the activity_calculator.xml

---
+++ src/dev/gokova/calculator/Calculator.java
@@ -0,0 +1,91 @@
+package dev.gokova.calculator;
+
+public abstract class Calculator {
+
+	public static Double calculate(Double operand1, Double operand2,
+			Operator operator) {
+		Double result = (double) 0;
+		switch (operator) {
+		case ADD:
+			result = Add(operand1, operand2);
+			break;
+		case SUBTRACT:
+			result = Subtract(operand1, operand2);
+			break;
+		case MULTIPLY:
+			result = Multiply(operand1, operand2);
+			break;
+		case DIVIDE:
+			result = Divide(operand1, operand2);
+			break;
+		case POWER:
+			result = Power(operand1, operand2);
+			break;
+		default:
+			result = null;
+			break;
+		}
+		return result;
+	}
+
+	public static Double calculate(Double operand, Operator operator) {
+		Double result = (double) 0;
+		switch (operator) {
+		case FACTORIAL:
+			result = Factorial(operand);
+			break;
+		case SQUAREROOT:
+			result = SquareRoot(operand);
+			break;
+		case SQUARE:
+			result = Square(operand);
+			break;
+		default:
+			result = null;
+			break;
+		}
+		return result;
+	}
+
+	private static Double Add(Double operand1, Double operand2) {
+		return operand1 + operand2;
+	}
+
+	private static Double Subtract(Double operand1, Double operand2) {
+		return operand1 - operand2;
+	}
+
+	private static Double Multiply(Double operand1, Double operand2) {
+		return operand1 * operand2;
+	}
+
+	private static Double Divide(Double operand1, Double operand2) {
+		return operand1 / operand2;
+	}
+
+	private static Double Factorial(Double operand) {
+		if (operand.equals(0)) {
+			return (double) 1;
+		} else if (operand.equals(2)) {
+			return (double) 1;
+		} else {
+			Double result = (double) 1;
+			for (int i = 2; i <= operand; i++) {
+				result *= operand;
+			}
+			return result;
+		}
+	}
+
+	private static Double SquareRoot(Double operand) {
+		return Math.sqrt(operand);
+	}
+
+	private static Double Square(Double operand) {
+		return Math.pow(operand, 2);
+	}
+
+	private static Double Power(Double operand1, Double operand2) {
+		return Math.pow(operand1, operand2);
+	}
+}
---
+++ src/dev/gokova/calculator/Operator.java
@@ -0,0 +1,5 @@
+package dev.gokova.calculator;
+
+public enum Operator {
+	ADD, SUBTRACT, MULTIPLY, DIVIDE, FACTORIAL, SQUAREROOT, SQUARE, POWER
+}
